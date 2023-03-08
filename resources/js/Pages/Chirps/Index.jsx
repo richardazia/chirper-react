f9e97f54c9d80c1b1cf0c1d83703c4402e880bf4
Richard Azia <richard@main-vision.com>
2023-03-08 16:18:03 +0100

    correct typos

--- resources/js/Pages/Chirps/Index.jsx
+++ resources/js/Pages/Chirps/Index.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import Inputerror from '@/Components/InputError';
+import InputError from '@/Components/InputError';
 import PrimaryButton from '@/Components/PrimaryButton';
-// import { useForm, Head } from '@inertiajs/react';
 import { useForm, Head } from '@inertiajs/react';
 
 export default function Index({ auth }) {
@@ -17,7 +16,7 @@ export default function Index({ auth }) {
 
     return (
         <AuthenticatedLayout auth={auth}>
-            <head title="Chirps"></head>
+            <Head title="Chirps" />
 
             <div className="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
                 <form onSubmit={submit}>
@@ -27,10 +26,10 @@ export default function Index({ auth }) {
                         className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
                         onChange={e => setData('message', e.target.value)}
                         ></textarea>
-                        <Inputerror message={error.message} className="mt-2" />
+                        <InputError message={errors.message} className="mt-2" />
                         <PrimaryButton className="mt-4" disabled={processing}>Chirp</PrimaryButton>
                 </form>
             </div>
         </AuthenticatedLayout>
-    )
+    );
 }
